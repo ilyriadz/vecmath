a0a07aab90220a8a64be73fc57e9fe3cb52018c4
ilyriadz <51867150+ilyriadz@users.noreply.github.com>
2022-01-21 23:03:58 +0100

    add source files and module-info
    this is the modular version of vecmath

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module javax.vecmath 
+{
+    requires java.desktop;
+    
+    exports javax.vecmath;
+    opens javax.vecmath;
+}
